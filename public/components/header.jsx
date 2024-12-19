d053cf09ab4b44b266f7b78aabb487c6b6fb6b8c
LiorRachi <liorrachi09@gmail.com>
2024-12-19 14:59:53 +0200

    fix issues from code review

--- public/components/header.jsx
+++ public/components/header.jsx
@@ -1,16 +1,15 @@
-import css from '../css/general.module.css'
+import css from '../css/header.module.css'
 
 export default function Header() {
     const currentDate = new Date().toLocaleDateString('en-gb', { day: "numeric", month: "numeric", year: "numeric" });
+    const title = "Pokèmon";
     return (
         <header className={css.header}>
             <div>
                 <img src="https://cdn-icons-png.flaticon.com/128/868/868596.png" alt="pokeball" />
-                <h1>Pokèmon</h1>
+                <h1>{title}</h1>
             </div>
-            <div>
-                <h1>{currentDate}</h1>
-            </div>
+            <h1>{currentDate}</h1>
         </header>
     )
 }
--- public/components/loginForm.jsx
+++ public/components/loginForm.jsx
@@ -1,24 +1,22 @@
 import css from '../css/login.module.css';
 export default function LoginForm() {
     return (
-        <main className={css.mainWrapper}>
+        <div className={css.componentWrapper}>
             <div className={css.blurryBackground}></div>
-            <section className={css.main}>
-                <section className={css.formWrapper}>
-                    <form className={css.form}>
-                        <h1 className={css.title}>L<span><img src="https://cdn-icons-png.flaticon.com/128/868/868596.png" /></span>gin</h1>
-                        <section className={css.inputs}>
-                            <input className={css.input} type="text" id="_username" placeholder="username..." />
-                            <input className={css.input} type="password" id="_password" placeholder="password..." />
-                            <h6 className={css.msg}></h6>
-                        </section>
-                        <section className={css.buttons}>
-                            <button className={css.button} id="login-btn" type="button">Login</button>
-                            <button className={css.button} id="join-as-guest-btn" type="button">Join As Guest</button>
-                        </section>
-                    </form>
-                </section>
-            </section>
-        </main>
+            <div className={css.formWrapper}>
+                <form>
+                    <h1>L<span><img src="https://cdn-icons-png.flaticon.com/128/868/868596.png" /></span>gin</h1>
+                    <section className={css.inputs}>
+                        <input type="text" id="_username" placeholder="username..." />
+                        <input type="password" id="_password" placeholder="password..." />
+                        <p></p>
+                    </section>
+                    <section className={css.buttons}>
+                        <button className={css.button} id="login-btn" type="button">Login</button>
+                        <button className={css.button} id="join-as-guest-btn" type="button">Join As Guest</button>
+                    </section>
+                </form>
+            </div>
+        </div>
     )
 } 
